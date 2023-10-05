1ed14a47c346a85b8855185773924119d6554807
tete-30 <hortensia_porto@hotmail.com>
2023-10-04 22:27:05 -0300

    Primeiro Commit

--- app/src/main/java/br/com/fundatec/fundatecheroti22/login/presentation/LoginViewModel.kt
+++ app/src/main/java/br/com/fundatec/fundatecheroti22/login/presentation/LoginViewModel.kt
@@ -3,14 +3,28 @@ package br.com.fundatec.fundatecheroti22.login.presentation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import br.com.fundatec.fundatecheroti22.R
+import br.com.fundatec.fundatecheroti22.login.presentation.model.LoginViewState
 
 class LoginViewModel : ViewModel() {
 
+    private val viewState: MutableLiveData<LoginViewState> = MutableLiveData()
+    val state: LiveData<LoginViewState> = viewState
+
+    private val passwordState: MutableLiveData<String> = MutableLiveData()
+    val password: LiveData<String> = passwordState
+
     fun validateInputs(email: String?, password: String?) {
-        Log.e("teste",email.toString())
-        Log.e("teste",password.toString())
+        if (email.isNullOrBlank()) {
+            viewState.value = LoginViewState.ShowEmailError
+        }
+
+        if (password.isNullOrBlank()) {
+            viewState.value = LoginViewState.ShowPasswordError
+        }
 
     }
 
--- app/src/main/java/br/com/fundatec/fundatecheroti22/login/view/LoginActivity.kt
+++ app/src/main/java/br/com/fundatec/fundatecheroti22/login/view/LoginActivity.kt
@@ -8,24 +8,54 @@ import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import br.com.fundatec.fundatecheroti22.R
 import br.com.fundatec.fundatecheroti22.databinding.ActivityLoginBinding
+import br.com.fundatec.fundatecheroti22.home.profile.view.ProfileActivity
 import br.com.fundatec.fundatecheroti22.home.view.HomeActivity
 import br.com.fundatec.fundatecheroti22.login.presentation.LoginViewModel
+import br.com.fundatec.fundatecheroti22.login.presentation.model.LoginViewState
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
     private val viewModel:LoginViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
-        binding = ActivityLoginBinding.inflate(layoutInflater)
         setTheme(R.style.Theme_FundatecHeroTI22)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val buttonLogin = findViewById<Button>(R.id.loginButton)
-        buttonLogin.setOnClickListener {
-            val intent = Intent(this@LoginActivity, HomeActivity::class.java)
-            startActivity(intent)
+        configloginButton()
+        configNewHereButton()
+
+        viewModel.state.observe(this) {
+            when (it) {
+                is LoginViewState.Success -> TODO()
+                is LoginViewState.Error -> TODO()
+                LoginViewState.Loading -> TODO()
+                LoginViewState.ShowEmailError ->
+                    showEmaigitlError()
+                LoginViewState.ShowPasswordError ->
+                    showPasswordError()
+    }
+
+    private fun configButtonLogin() {
+        binding.loginButton.setOnClickListener {
+            viewModel.validateInputs(
+                binding.email.text.toString(),
+                binding.password.text.toString()
+            )
+//            startActivity(Intent(this@LoginActivity, HomeActivity::class.java))
         }
+    }
 
+    private fun configNewHereButton(){
+        binding.newHereTextView.setOnClickListener {
+            startActivity(Intent(this@LoginActivity, ProfileActivity::class.java))
+
+        }
+    }
+    private fun showEmailError(errorMessage: String){
+        binding.email.error = errorMessage
+    }
+    private fun showPasswordError(errorMessage: String){
+        binding.password.error = errorMessage
     }
 }
